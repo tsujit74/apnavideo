6b3dbb29a5afd695f3756873d043f41a4f8dc548
Sujit Thakur <tsujeet440@gmail.com>
2024-10-04 21:41:27 +0530

    path

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,9 +3,6 @@ dotenv.config();
 
 import express from "express";
 import { createServer } from "node:http";
-import { fileURLToPath } from 'url';
-import {join} from 'path';
-import path from "node:path";
 
 import { Server } from "socket.io";
 
@@ -24,17 +21,6 @@ app.set("port", process.env.PORT || 5500);
 app.use(cors());
 app.use(express.json({limit:"40kb"}));
 app.use(express.urlencoded({limit:"40kb", extended: true}));
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(join(__dirname, 'build')));
-
-
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 app.use("/api/v1/users",userRoutes);
 

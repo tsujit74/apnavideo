87cb417a7b4b00e257a92a12dc52d8988aedd8f7
Sujit Thakur <tsujeet440@gmail.com>
2024-10-06 15:57:34 +0530

    Password lenght issue

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -50,7 +50,7 @@ const register = async (req, res) => {
         .json({ message: "user already exists" });
     }
 
-    if (password.length <= 6) {
+    if (password.length < 6) {
       return res
         .status(httpStatus.LENGTH_REQUIRED)
         .json({ message: "Passwod should be 6 Letter" });

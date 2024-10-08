2ed236570229ad90547dc952ee866937d42741e4
Sujit Thakur <tsujeet440@gmail.com>
2024-10-08 18:50:50 +0530

    Notification for sucess password

--- frontend/my-vite-app/src/pages/ResetPassword.jsx
+++ frontend/my-vite-app/src/pages/ResetPassword.jsx
@@ -5,8 +5,12 @@ import { useParams, useNavigate } from "react-router-dom";
 import "../styles/ResetPassword.css";
 import server from "../environment";
 import CircularProgress from "@mui/material/CircularProgress";
+import { useSnackbar } from "notistack";
 
 const ResetPassword = () => {
+
+  const {enqueueSnackbar} = useSnackbar();
+
   const [email, setEmail] = useState('');
   const [code, setCode] = useState('');
   const [password, setPassword] = useState('');
@@ -19,8 +23,18 @@ const ResetPassword = () => {
     e.preventDefault();
 
     try {
+
+      if (!email || !password || !code) {
+        enqueueSnackbar("All fields are required", {
+          variant: "error",
+          anchorOrigin: { vertical: "top", horizontal: "center" },
+          autoHideDuration: 2000,
+        });
+        return;
+      }
+
       setLoading(true);
-      const response = await axios.post(`${server}/resetPassword`, {
+      const response = await axios.post(`${server}/api/v1/users/resetPassword`, {
         email,
         code,
         password,
@@ -28,8 +42,19 @@ const ResetPassword = () => {
       setMessage(response.data.message);
       setOpen(true);
       setTimeout(() => navigate("/auth"), 2000);
+      enqueueSnackbar("Password Update successfully!", {
+        variant: "success",
+        anchorOrigin: { vertical: "top", horizontal: "center" },
+        autoHideDuration: 2000,
+      });
+  
     } catch (error) {
       setMessage(error.response.data.message || "An error occurred.");
+      enqueueSnackbar("Password Not Update", {
+        variant: "error",
+        anchorOrigin: { vertical: "top", horizontal: "center" },
+        autoHideDuration: 2000,
+      });
       setOpen(true);
     } finally {
       setLoading(false);
@@ -55,11 +80,10 @@ const ResetPassword = () => {
           <div className="form-group">
           <label htmlFor="email">Email:</label>
           <input
-        type="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         placeholder="Enter your email"
-        required
+      
       />
       <label htmlFor="code">Reset Code:</label>
       <input
@@ -67,7 +91,7 @@ const ResetPassword = () => {
         value={code}
         onChange={(e) => setCode(e.target.value)}
         placeholder="Enter your reset code"
-        required
+      
       />
             <label htmlFor="password">New Password:</label>
             <input
@@ -76,7 +100,7 @@ const ResetPassword = () => {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               placeholder="New Password"
-              required
+
             />
           </div>
           <button type="submit">Reset Password</button>
